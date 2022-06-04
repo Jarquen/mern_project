dd2e9b0e99c46ec3d01a10898de83848b08f53d4
Quentin <quentin.jarlot@gmail.com>
2022-06-04 20:18:10 +0200

    Add pagination

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,14 +1,19 @@
 import React, {useEffect, useState} from 'react';
-import {Container, Grid, Grow} from "@material-ui/core";
+import {Container, Grid, Grow, Paper} from "@material-ui/core";
 import Posts from "../Posts/Posts";
 import Form from "../Form/Form";
 import {useDispatch} from "react-redux";
 import * as api from "../../api";
 import {getPosts} from "../../feature/postsSlice";
+import Pagination from "../Pagination";
+import useStyles from './styles';
+
 
 const Home = () => {
     const [currentId, setCurrentId] = useState(0)
     const dispatch = useDispatch();
+    const classes = useStyles()
+
 
     useEffect(() => {
         api.fetchPosts().then((res) => dispatch(getPosts(res.data)));
@@ -23,6 +28,9 @@ const Home = () => {
                     </Grid>
                     <Grid item xs={12} sm={4}>
                         <Form currentId={currentId} setCurrentId={setCurrentId}/>
+                        <Paper className={classes.pagination} elevation={6}>
+                            <Pagination />
+                        </Paper>
                     </Grid>
                 </Grid>
             </Container>
