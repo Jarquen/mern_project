4d2dfcc797ac25a7798d21138474337391476084
Quentin <quentin.jarlot@gmail.com>
2022-06-09 17:20:00 +0200

    Finish project

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import useStyles from './styles';
 import {Card, CardActions, CardContent, Button, Typography, CardMedia, ButtonBase} from "@material-ui/core";
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
@@ -16,16 +16,19 @@ const Post = ({post, setCurrentId}) => {
     const dispatch = useDispatch()
     const user = JSON.parse(localStorage.getItem('profile'));
     const navigate = useNavigate();
+    const [likes, setLikes] = useState(post?.likes);
+    const hasLikedPost = post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id));
+    const userId = user?.result.googleId || user?.result._id;
 
     const Likes = () => {
-        if (post.likes.length > 0) {
-            return post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id))
+        if (likes.length > 0) {
+            return likes.find((like) => like === userId)
                 ? (
                     <><ThumbUpAltIcon
-                        fontSize="small"/>&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}`}</>
+                        fontSize="small"/>&nbsp;{likes.length > 2 ? `You and ${likes.length - 1} others` : `${likes.length} like${likes.length > 1 ? 's' : ''}`}</>
                 ) : (
                     <><ThumbUpAltOutlined
-                        fontSize="small"/>&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}</>
+                        fontSize="small"/>&nbsp;{likes.length} {likes.length === 1 ? 'Like' : 'Likes'}</>
                 );
         }
 
@@ -47,6 +50,11 @@ const Post = ({post, setCurrentId}) => {
             await api.likePost(post._id).then(() => {
                 dispatch(likePost(post._id))
             });
+            if (hasLikedPost) {
+                setLikes(post.likes.filter((id) => id !== userId))
+            } else {
+                setLikes([...post.likes, userId])
+            }
         } catch (error) {
             console.log(error)
         }
